6db54ee6c5e5ea0120546874bd6f88494ac2befc
Biswojit jena <140076559+Biswojit123@users.noreply.github.com>
2024-03-12 12:03:39 +0530

    Predicate use for pailndrom

--- Practice-DSA/src/Paliandrom.java
+++ Practice-DSA/src/Paliandrom.java
@@ -1,6 +1,8 @@
 import java.io.*;
+import java.util.function.Predicate;
 
 public class Paliandrom {
+    //That is the method call
     public static boolean palindromChecking(int arr[]) {
         boolean blu = true;
         for(int i = 0 ; i < arr.length ; i ++) {
@@ -17,7 +19,7 @@ public class Paliandrom {
         try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             System.out.println("Enter the size of array :");
             int size = Integer.parseInt(br.readLine());
-            int arr[] = new int[size];
+            Integer[] arr = new Integer[size];
 
             System.out.println("Enter the element of array :");
             for(int i = 0 ; i < size ; i++) {
@@ -25,13 +27,32 @@ public class Paliandrom {
             }
 
             //Now we check the element of array is palindrom are not
-            boolean bul = palindromChecking(arr);
+            //boolean bul = palindromChecking(arr);
 
-            if(bul == true) {
+
+            //Here i use the predicate functional interface
+            Predicate<Integer []> bul = array-> {
+                boolean blu = true;
+                for(int i = 0 ; i < array.length ; i ++) {
+                    if( array[i] == array[ array.length - 1 -i ] )  {
+                        continue;
+                    } else {
+                        blu = false;
+                        return blu;
+                    }
+                }
+                return blu;
+            };
+
+            boolean b = bul.test(arr);
+
+            if(b == true) {
                 System.out.print("Given array is palindrom!");
             }else {
                 System.out.println("Given array is not palindrom!");
             }
+
+            
         }catch(Exception e) {
             e.printStackTrace();
         }
